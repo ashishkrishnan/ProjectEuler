1fb18fb73c969ad7c5e20585c1ec5c2725bb34e9
Ashish Krishnan <ashishkrish09@gmail.com>
2015-08-20 22:52:32 +0530

    Fixes

--- src/org/askdn/euler/Euler1_Mutliples.java
+++ src/org/askdn/euler/Euler1_Mutliples.java
@@ -15,21 +15,21 @@ public class Euler1_Mutliples {
         N--;
         long sum =0;
         long num = 0;
-        //for numbers that is divisble by 3
+        //for numbers that is divisible by 3
         if(N>=3)
         {
-         num = N/3;
+         num = N/3; // calculating the number of terms divisible by 3 under N
          sum+=num * (3 + num*3)/2;
         }
         if(N>=5)
         {
-         num = N/5;
+         num = N/5; // calculating the number of terms divisible by 5 under N
          sum+=num * (5 + num*5)/2;
         }
          if(N>=15)
         {
-         num = N/15;
-         sum-=num * (15 + num*15)/2;
+         num = N/15; // calculating the number of terms divisible by 15 under N
+         sum-=num * (15 + num*15)/2; // difference of the sum 
         }
        
        System.out.println(sum);
